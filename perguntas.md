1fcc425aca7784f04228778c0a951e2d561a9667
Diego Carlos Reis da Vitoria <53801283+DiegodaVitoria@users.noreply.github.com>
2021-05-23 15:18:13 -0300

    done

---
+++ perguntas.md
@@ -0,0 +1,40 @@
+Quem foi a primeira pessoa a viajar no Espaço?
+a) Yuri Gagarin
+b) A cadela Laika
+c) Neil Armstrong
+d) Marcos Pontes
+e) Buzz Aldrin
+
+Qual a montanha mais alta do mundo?
+a) Mauna Kea
+b) Dhaulagiri
+c) Monte Chimborazo
+d) Monte Everest
+e) Pico da Neblina
+
+Quem inventou a lâmpada?
+Graham Bell
+Steve Jobs
+Thomas Edison
+Henry Ford
+Santos Dumont
+
+A que temperatura a água ferve?
+200 ºC
+180 ºC
+100 ºC
+
+
+Quantos ossos temos no nosso corpo?
+a) 126
+b) 206
+c) 18
+d) 300
+e) 200
+
+Que animal põe o maior ovo?
+Beija-flor
+Avestruz
+Tartaruga-gigante
+Galinha
+Pavão
---
+++ questions.js
@@ -0,0 +1,56 @@
+//criar um array onde passa os numeros, questoes, opcoes e resposta
+let questions = [
+ {
+  numb: 1,
+  question: "Quem foi a primeira pessoa a viajar no Espaço?",
+  answer: "Neil Armstrong",
+  options: [
+   "Yuri Gagarin",
+   "A cadela Laika",
+   "Neil Armstrong",
+   "Marcos Pontes"
+  ]
+ },
+ {
+  numb: 2,
+  question: "Qual a montanha mais alta do mundo?",
+  answer: "Monte Everest",
+  options: [
+   "Dhaulagiri",
+   "Monte Chimborazo",
+   "Monte Everest",
+   "Pico da Neblina"
+  ]
+ },
+ {
+  numb: 3,
+  question: "Quem inventou a lâmpada?",
+  answer: "Thomas Edison",
+  options: [
+   "Steve Jobs",
+   "Thomas Edison",
+   "Henry Ford",
+   "Santos Dumont"
+  ]
+ }, {
+  numb: 4,
+  question: "A que temperatura a água ferve?",
+  answer: "100 ºC",
+  options: [
+   "200 ºC",
+   "98,7 ºC",
+   "100 ºC",
+   "102,3 ºC"
+  ]
+ }, {
+  numb: 5,
+  question: "Que animal põe o maior ovo?",
+  answer: "Tartaruga-gigante",
+  options: [
+   "Beija-flor",
+   "Avestruz",
+   "Tartaruga-gigante",
+   "Galinha"
+  ]
+ },
+]
